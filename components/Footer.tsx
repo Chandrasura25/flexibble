f9f5efa286daf97d7ab62c70945226f37b14d949
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-31 16:27:16 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";  
 const Footer = () => {
   return (
     <footer className="flexStart footer">
         <div className="flex flex-col gap-12 w-full">
-            <div className=""></div>
+            <div className="flex items-start flex-col">
+                <Image src="/logo-purple.svg" width={115} height={38} alt="flexibble" />
+                <p className="text-start tesxt-sm font-normal mt-5 max-w-xs">Flexibble</p>
+            </div>
         </div>
     </footer>
   )
