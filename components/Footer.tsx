70cc050d9c5a7afd8ee2024c4336aa6baa383324
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-31 16:45:30 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,24 @@
 import { footerLinks } from "@/constant";
 import Image from "next/image";
+import Link from "next/link";
 
 type ColumnProps = {
-    title: string;
-    links : Array<string>;
-}
+  title: string;
+  links: Array<string>;
+};
 
-const FooterColumn = ({title, links} : ColumnProps) => (
-    <div className="footer_column">
-        <h4 className="font-semibold">{title}</h4>
-        <ul className="flex flex-col gap-2 font-normal"></ul>
-    </div>
-)
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer_column">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
 
 const Footer = () => {
   return (
@@ -30,7 +37,10 @@ const Footer = () => {
           </p>
         </div>
         <div className="flex flex-wrap gap-12">
-            <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
         </div>
       </div>
     </footer>
