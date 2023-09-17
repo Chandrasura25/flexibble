d1de62019535e2c66c136b7756cc6ac1d2244f94
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-09-17 18:57:53 +0100

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,9 @@ import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "@/lib/session";
 
 const Navbar = async () => {
-  const session = await getCurrentUser()
+  const session = await getCurrentUser();
   return (
-    <nav className="flexBetween navbar"> 
+    <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
         <Link href="/">
           <Image src="/logo.svg" width={115} height={43} alt="flexibble" />
@@ -21,9 +21,17 @@ const Navbar = async () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            {session?.user?.image && (
+              <Image
+                src={session?.user?.image}
+                width={40}
+                height={40}
+                alt={session?.user?.name}
+                className="rounded-full"
+              />
+            )}
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
